a47b33bc0e320bcc7832fbd1fd5ce416f1724d11
Paulo Vieira <80427803+paulovieirajr@users.noreply.github.com>
2025-05-15 10:42:30 -0300

    chore: Init project

---
+++ src/main/java/io/github/paulovieirajr/estapar/EstaparApplication.java
@@ -0,0 +1,13 @@
+package io.github.paulovieirajr.estapar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstaparApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstaparApplication.class, args);
+	}
+
+}
